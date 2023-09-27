57d60a79a50702f3af914cc9fd3f390585d2f022
Thirdyyyyyyy <thirdyyy07@outlook.com>
2023-09-28 00:49:19 +0800

    Layout.js: Rename child to children

--- components/Layout.js
+++ components/Layout.js
@@ -13,13 +13,13 @@ import Nav from '../components/Nav';
 import Header from '../components/Header';
 import TopLeftImg from '../components/TopLeftImg';
 
-const Layout = ({ child }) => {
+const Layout = ({ children }) => {
   return (
     <div className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}>
       <TopLeftImg />
       <Nav />
       <Header />
-      {child}
+      {children}
     </div>
   )
 };
